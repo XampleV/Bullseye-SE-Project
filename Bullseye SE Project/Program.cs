594746c56e02736e8b88ac818cae2670da1cee12
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-09-24 11:34:25 -0400

    aa

--- Bullseye SE Project/Program.cs
+++ Bullseye SE Project/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Bullseye_SE_Project
+namespace Bullseye_Project
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Introduction();
+        }
+
+        static void Introduction()
+        {
+            // we'll welcome the user here, and ask them are you ready to play or something
+            // and we'll call functions.mainFunctions.MainFuncionEntry();
         }
     }
 }
---
+++ Bullseye SE Project/functions/mainFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+namespace Bullseye_Project.functions
+{
+    class mainFunctions
+    {
+        public static Random _random = new Random(); // This will be our random generator variable.
+        // we don't need 2 values for x,y we can grab like this:
+        // for X: Cursor.Position.X
+        // for Y: Cursor.Position.Y
+
+        static public void MainFuncionEntry()
+        {
+            // ready the threads
+            Thread currentPositionLoop = new Thread(new ThreadStart(HowCloseThread));
+
+
+            // start all threads
+            currentPositionLoop.Start();
+
+        }
+        static public void HowCloseThread()
+        {
+            //Here we'll loop constantly, I'll grab the position of the mouse and calculate the pixels.
+            Console.WriteLine(Cursor.Position.X);
+        }
+    }
+}
