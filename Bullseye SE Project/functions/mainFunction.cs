f619994d2d5c05d9912470d81b5e05dd082562be
smittyy <57191850+smittyy@users.noreply.github.com>
2020-09-29 11:24:19 -0400

    8

--- Bullseye SE Project/functions/mainFunction.cs
+++ Bullseye SE Project/functions/mainFunction.cs
@@ -23,99 +23,88 @@ namespace Bullseye_Project.functions
         // for X: Cursor.Position.X
         // for Y: Cursor.Position.Y
 
-        static public void MainFunctionEntry()
+        static public void MainFuncionEntry()
         {
             //Make the Goal the First Time
             GoalPoint();
 
             // ready the threads
-            Thread currentPositionLoop = new Thread(new ThreadStart(HowCloseThread));
             //Thread currentGoal = new Thread(new ThreadStart(GoalPoint));
             Thread howPlayerDoing = new Thread(new ThreadStart(CloseOrNot));
 
-      
-                // start all threads
-                currentPositionLoop.Start();
-                //currentGoal.Start();
-                howPlayerDoing.Start();
+
+            // start all threads
+            //currentGoal.Start();
+            howPlayerDoing.Start();
 
 
         }
 
-        public static void HowCloseThread()
-        {
-            //Here we'll loop constantly, I'll grab the position of the mouse and calculate the pixels.
-            while (true)
-            {
-                mouseXPos = Cursor.Position.X;
-                mouseYPos = Cursor.Position.Y;
-                //Console.WriteLine(Cursor.Position.Y);
-                Thread.Sleep(30);
-                //push
-            }
-        }
+
 
         //Find the goal point to hover at
         public static void GoalPoint()
         {
 
-                //Generate two seperate points set as the goal and then hand to the player
-                goalXPos = _random.Next(1920);
-                goalYPos = _random.Next(1080);
-                Console.WriteLine($"The goal is x{goalXPos} y{goalYPos}");
-
+            //Generate two seperate points set as the goal and then hand to the player
+            goalXPos = _random.Next(1920);
+            goalYPos = _random.Next(1080);
+            Console.WriteLine($"The goal is x{goalXPos} y{goalYPos}");
         }
 
         //how well the palyer did
         public static void CloseOrNot()
         {
-            while (true) {
-                scoreStuffX = goalXPos - mouseXPos;
-                scoreStuffY = goalYPos - mouseYPos;
-
-                if (scoreStuffX >= 1000 && scoreStuffY >=1000)
-                {
-                    Console.WriteLine("Too Cold.");
-                } else if (scoreStuffX >= 600 && scoreStuffY >= 600)
-                {
-                    Console.WriteLine("Warmer");
-                } else if (scoreStuffX >= 200 && scoreStuffY >= 200)
-                {
-                    Console.WriteLine("HOT!");
-                }
-                else if (scoreStuffX >= 60 && scoreStuffY >= 60)
-                {
-                    Console.WriteLine("Super HOT!");
-                }
-                else if (scoreStuffX >= 30 && scoreStuffY >= 30)
-                {
-                    Console.WriteLine("Winner");
-                    PlayAgain();
-                }
-
-                Thread.Sleep(2000);
-            }
-        }
-
-        //Play again or not
-        public static void PlayAgain()
-        {
-            Thread currentPositionLoop = new Thread(new ThreadStart(HowCloseThread));
-            Thread howPlayerDoing = new Thread(new ThreadStart(CloseOrNot));
-
-            Console.WriteLine("If you want to play again please type yes. If not just type no.");
-            playAgain = Console.ReadLine().ToLower();
-
-            if (playAgain == "yes")
+            while (true)
             {
-                Console.WriteLine("Then lets go.");
-                MainFunctionEntry();
-            }
-            else
-            {
-                Console.WriteLine("Thats too bad. Guess I'll cya later.");
-                howPlayerDoing.Abort();
-                currentPositionLoop.Abort();
+                Thread.Sleep(100);
+                int heyX = Cursor.Position.X;
+                int heyY = Cursor.Position.Y;
+                scoreStuffX = goalXPos - heyX;
+                scoreStuffY = goalYPos - heyY;
+
+                if (scoreStuffX < 0)
+                {
+                    scoreStuffX = scoreStuffX * -1;
+                }
+                if (scoreStuffY < 0)
+                {
+                    scoreStuffY = scoreStuffY * -1;
+                }
+
+
+                if (scoreStuffX <= 30 && scoreStuffY <= 30)
+                {
+                    Console.WriteLine($"YOU GOT IT\ngoal:  x{goalXPos} y{goalYPos}\ncurrent: {heyX},{heyY}\nFar away values: {scoreStuffX},{scoreStuffY}\n------------");
+                    Console.ReadLine();
+                    Console.Clear();
+                    GoalPoint();
+                }
+                else if (scoreStuffX <= 100 && scoreStuffY <= 100)
+                {
+                    Console.WriteLine($"HOT!\ngoal:  x{goalXPos} y{goalYPos}\ncurrent: {heyX},{heyY}\nFar away values: {scoreStuffX},{scoreStuffY}\n------------");
+
+                }
+                else if (scoreStuffX <= 600 && scoreStuffY <= 600)
+                {
+                    Console.WriteLine($"Warmer\ngoal:  x{goalXPos} y{goalYPos}\ncurrent: {heyX},{heyY}\nFar away values: {scoreStuffX},{scoreStuffY}\n------------");
+                }
+                else if (scoreStuffX <= 1000 && scoreStuffY <= 1000)
+                {
+                    Console.WriteLine($"You're too cold.\ngoal:  x{goalXPos} y{goalYPos}\ncurrent: {heyX},{heyY}\nFar away values: {scoreStuffX},{scoreStuffY}\n------------");
+                }
+                else
+                {
+                    Console.WriteLine($"You're frozen.\ngoal:  x{goalXPos} y{goalYPos}\ncurrent: {heyX},{heyY}\nFar away values: {scoreStuffX},{scoreStuffY}\n------------");
+                }
+
+
+
+
+
+
+
+
             }
         }
     }
